94f53e5ecb783889c2cdd17aecc9601cf72f1bba
SaurokiFestus <saurokisaigilu.45@gmail.com>
2024-11-25 12:19:12 +0300

    changes

--- website1/auth.py
+++ website1/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "Login"
+    return render_template('login.html', boolean=False)
 
 @auth.route('/logout')
 def logout():
-    return 'Logout'
+    return render_template('logout.html')
 
 @auth.route('/signup')
 def signup():
-    return 'signup'
+    return render_template('signup.html')
